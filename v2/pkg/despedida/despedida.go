32f3e59d0fe171b26a9c310c683ebedc57171e74
devlatterpay <developer@latterpay.com>
2020-06-24 18:03:30 +0200

    Add functionality with new despedida msg

--- v2/pkg/despedida/despedida.go
+++ v2/pkg/despedida/despedida.go
@@ -1,6 +1,18 @@
 package despedida
 
+import (
+	"fmt"
+
+	"github.com/programatta/midemolib01/internal"
+)
+
 // DespedidaSimple returns a single message
 func DespedidaSimple() string {
 	return "Adios y gracias por usarme"
+}
+
+// DespedidaType1 retuns a single message with MsgType1
+func DespedidaType1() string {
+	m := internal.MsgType1()
+	return fmt.Sprintf("Adios y gracias con un msgType1:%s", m)
 }
--- v2/tests/despedida_test.go
+++ v2/tests/despedida_test.go
@@ -1,14 +1,24 @@
 package tests
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/programatta/midemolib01/internal"
 	despedidav2 "github.com/programatta/midemolib01/v2/pkg/despedida"
 )
 
 func TestDespedidaSimple(t *testing.T) {
 	expected := "Adios y gracias por usarme"
 	if msg := despedidav2.DespedidaSimple(); msg != expected {
+		t.Errorf("DespedidaSimple() returns \"%s\" and expected \"%s\"", msg, expected)
+	}
+}
+
+func TestDespedidaType1(t *testing.T) {
+	msg1 := internal.MsgType1()
+	expected := fmt.Sprintf("Adios y gracias con un msgType1:%s", msg1)
+	if msg := despedidav2.DespedidaType1(); msg != expected {
 		t.Errorf("DespedidaSimple() returns \"%s\" and expected \"%s\"", msg, expected)
 	}
 }
